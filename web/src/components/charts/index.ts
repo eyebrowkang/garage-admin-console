017b93caac5408bccbbde6dd67d3d480e3659d3c
eyebrowkang <me@eyebrowkang.com>
2026-02-04 16:58:02 +0800

    Remove orphaned web files

--- web/src/components/charts/index.ts
+++
@@ -1,4 +0,0 @@
-export { ClusterHealthChart } from './ClusterHealthChart';
-export { NodeStatusChart } from './NodeStatusChart';
-export { CapacityGauge } from './CapacityGauge';
-export { PartitionChart } from './PartitionChart';
--- web/src/components/cluster/index.ts
+++
@@ -1,6 +0,0 @@
-export { ConfirmDialog, type ConfirmTier } from './ConfirmDialog';
-export { PageHeader } from './PageHeader';
-export { EmptyState } from './EmptyState';
-export { NodeSelector } from './NodeSelector';
-export { SecretReveal } from './SecretReveal';
-export { JsonViewer } from './JsonViewer';
