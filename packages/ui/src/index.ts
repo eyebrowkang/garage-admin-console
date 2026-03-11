b04f4ec0c36007db7c656ac3478389b103eafab3
eyebrowkang <me@eyebrowkang.com>
2026-03-11 08:29:38 +0800

    chore: add @garage-admin/ui shared package
    Provides cn() utility and basic shadcn/ui components (Button, Card)
    for use by workspace packages.

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,10 @@
+export { cn } from './lib/utils.js';
+export { Button, buttonVariants } from './components/button.js';
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from './components/card.js';
